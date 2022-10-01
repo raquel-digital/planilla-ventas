3432474083f972c892f0e6be808178d84132fbec
raquel-digital <93403242+raquel-digital@users.noreply.github.com>
2022-10-01 10:05:31 -0300

    res prob 1-10-2022

--- utils/midleware.js
+++ utils/midleware.js
@@ -13,11 +13,11 @@ const middlewares = {
         if(admin.usuario == log.usuario && admin.contraseña == log.contraseña ){
             
             adminOk = true;
-            return next()
+            return res.end()
         }
         if(user.usuario == log.usuario && user.contraseña == log.contraseña){
             loginOk = true;
-            return next();
+            return res.end();
         }else{
             return  res.redirect('/login');//
         } 
